ad6de11b738c59348a27ce60e1a2e8b76045a684
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2026-05-20 22:22:39 -0500

    incomplete program for lab

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("Select your first value: "))          # Ask for first value
+print(num1)
+print()
+num2 = int(input("Select your second value: "))         # Ask for second value
+print(num2)
+print()
+print("Choose an operation (+, -, *, /): ")             # As for operation to perform
+print()
+addition = input("")
+subtraction = input("")
+multiply = input("")
+division = input("")
