6aadefbd53ddbee6cb142e401fa5a87f46269204
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2026-05-21 16:48:16 -0500

    Calculator python file unoriginal source - internet based

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,4 +1,4 @@
-num1 = int(input("Select your first value: "))          # Ask for first value
+"""num1 = int(input("Select your first value: "))          # Ask for first value
 print(num1)
 print()
 num2 = int(input("Select your second value: "))         # Ask for second value
@@ -6,8 +6,42 @@ print(num2)
 print()
 print("Choose an operation (+, -, *, /): ")             # As for operation to perform
 print()
-"""addition = input("")
+addition = input("")
 subtraction = input("")
 multiply = input("")
 division = input("")"""
 print("End.")
+
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+print("Please select operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+sel = int(input("Select operation (1-4): "))
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+if sel == 1:
+    print(n1, "+", n2, "=", add(n1, n2))
+elif sel == 2:
+    print(n1, "-", n2, "=", sub(n1, n2))
+elif sel == 3:
+    print(n1, "*", n2, "=", mul(n1, n2))
+elif sel == 4:
+    print(n1, "/", n2, "=", div(n1, n2))
+else:
+    print("Invalid input")
